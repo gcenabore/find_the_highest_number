999c08758e6525ce22ffb8136afc2b454f3d821e
gcenabore <gianenabore30@gmail.com>
2024-10-15 17:25:23 +0800

    pseudo code

---
+++ find_highest_number.py
@@ -0,0 +1,5 @@
+#ask user to input 5 random numbers, and store them in variables
+#assume the first number is the largest. 
+#if a number is larger than the current "largest", the "largest" variable is updated
+#we compare the largest with the other variables
+#output the largest number 
